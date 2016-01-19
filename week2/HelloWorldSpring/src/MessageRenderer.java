2094988e984ec991efcbeba6588725313c1b765e
Ambrose <a.cole@mail.cryst.bbk.ac.uk>
2016-01-19 22:14:57 +0000

    week2 HelloWorldSpring

---
+++ week2/HelloWorldSpring/src/MessageRenderer.java
@@ -0,0 +1,10 @@
+/**
+ * Created by digibrose on 19/01/2016.
+ */
+public interface MessageRenderer {
+
+    public void render();
+    public void setMessageProvider(MessageProvider provider);
+    public MessageProvider getMessageProvider();
+}
+
