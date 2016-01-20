77b16b266609cf1e6d236c6b55e1189b858b82f7
BBK-SDP-2016-bcole04 <digibrose@gmail.com>
2016-01-20 15:14:56 +0000

    week2 HelloWorldSpringwith DIXMLwithConstructorArguments

---
+++ week2/HelloWorldSpXMLConst/src/ConfigurableMessageProvider.java
@@ -0,0 +1,14 @@
+/**
+ * Created by digibrose on 20/01/2016.
+ */
+public class ConfigurableMessageProvider implements MessageProvider{
+
+    private String message;
+    public ConfigurableMessageProvider(String message) {
+        this.message = message;
+    }
+    public String getMessage() {
+        return message;
+    }
+
+}
